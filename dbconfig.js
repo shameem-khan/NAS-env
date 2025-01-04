102d33395cf0f6a349e74cc548e289555122da13
shameem-khan <111282647+shameem-khan@users.noreply.github.com>
2025-01-04 13:28:46 +0400

    Add files via upload

---
+++ dbconfig.js
@@ -0,0 +1,18 @@
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();  
+
+const { Pool } = pg
+ 
+const db = new Pool({
+  host: process.env.DBConnLink_ext,
+  user: process.env.DBuser,
+  password: process.env.DBpass,
+  port: process.env.DBport,
+  database: "nas_test",
+  max: 20,
+  idle_timeout: 5,
+  ssl: true
+});
+
+export default db;
